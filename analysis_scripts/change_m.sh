052d3a719c36edb624839196aa40da42c73adb76
YurunT <944461466@qq.com>
2020-10-25 17:42:46 +0700

    modify tnn

--- analysis_scripts/change_m.sh
+++ analysis_scripts/change_m.sh
@@ -8,5 +8,5 @@ source activate ytian
 for p_mask in 0.45
 do
 	echo $p_mask
-    time python analysis.py -modelname mask -itemname pe -m $p_mask -T 0.6 -tm1 0.3 -tm2 0.7 -mind 10 -maxd 10 -ns 1 -nc 40 -change 0 -msg changemdl_8to10 -mdl  8 9 10
+    time python analysis.py -modelname mask -itemname pe -m $p_mask -T 0.6 -tm1 0.3 -tm2 0.7 -mind 1 -maxd 1 -ns 1 -nc 40 -change 0 -msg testtnn #-mdl  8 9 10
 done
--- auxiliary_scripts/tnn.py
+++ auxiliary_scripts/tnn.py
@@ -1,18 +1,42 @@
-def generate_new_transmissibilities_mask(T_mask1, T_mask2, T, m):
-    T1 = T * T_mask1
-    T2 = T * T_mask1 * T_mask2
-    T3 = T
-    T4 = T * T_mask2
+def generate_new_transmissibilities_mask(effeciency_list, T,):
+    '''
+    Input:  
+            effeciency_list: List of (inward effeciency(notated as 1), outward effeciency(notated as 2))
+            T: Orignial transmissibility of the virus
+    Output: 
+            List of Tij
+    '''
+#     T1 = T * T_mask1 #T12
+#     T2 = T * T_mask1 * T_mask2 #T11
+#     T3 = T #T22
+#     T4 = T * T_mask2 #T21
 
-    trans_dict = {'T1': T1,
-                  'T2': T2,
-                  'T3': T3,
-                  'T4': T4}
+#     trans_dict = {'T1': T1,
+#                   'T2': T2,
+#                   'T3': T3,
+#                   'T4': T4}
+#     print(trans_dict)
+
+#     effeciency_list = [[T_mask1, T_mask2],
+#                        [1, 1]]
+    typen = len(effeciency_list)
     
+    T_list = []
+    for u in range(typen):
+        u_row = []
+        for v in range(typen):
+            Tij = effeciency_list[u][0] * effeciency_list[v][1] * T
+            u_row.append(Tij)
+        T_list.append(u_row)
+    trans_dict = {'T1': T_list[0][1],
+              'T2': T_list[0][0],
+              'T3': T_list[1][1],
+              'T4': T_list[1][0]}    
+    print(trans_dict)
     return trans_dict    
 
-def generate_new_transmissibilities_mutation(T_mask1, T_mask2, T, m):
-    trans_dict = generate_new_transmissibilities_mask(T_mask1, T_mask2, T, m)
+def generate_new_transmissibilities_mutation(effeciency_list, T, m):
+    trans_dict = generate_new_transmissibilities_mask(effeciency_list, T,)
     T1 = trans_dict['T1']
     T2 = trans_dict['T2']
     T3 = trans_dict['T3']
