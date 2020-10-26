8a96793bfa00d97f222c7b4c5bfe4060c203b5b7
YurunT <944461466@qq.com>
2020-10-26 22:32:35 +0700

    add is_intermediete to mask es

--- theories/mask.py
+++ theories/mask.py
@@ -26,52 +26,64 @@ def P_A_given_R(i, T_list, k0, k1):
     assert res <= 1, "P_A_given_R should be less than 1"
     return res
 
-def P_A_given_B_N(i, k, n, T_list, A_0, A_1):
+def P_A_given_B_N(i, is_intermediate, k, n, T_list, A_0, A_1):
     one_minus_A0 = 1 - A_0
     one_minus_A1 = 1 - A_1
     
     p_abn = 0
     
+    if is_intermediate:
+        k1_range = k - n
+    else:
+        k1_range = k + 1 - n
+    
     for k0 in range(n + 1):
-        for k1 in range(k - n):
+        for k1 in range(k1_range):
             p_a_given_r = P_A_given_R(i, T_list, k0, k1)
             p_abn += p_a_given_r * \
                      comb(n, k0) * \
-                     comb(k - 1 - n, k1) * \
+                     comb(k1_range - 1, k1) * \
                      (A_0 ** k0) * \
                      (A_1 ** k1) * \
                      (one_minus_A0 ** (n - k0)) * \
-                     (one_minus_A1 ** (k - 1 - n - k1))
+                     (one_minus_A1 ** (k1_range - 1 - k1))
     return p_abn
 
-def P_A_given_B(i, k, T_list, A_0, A_1, m):
+def P_A_given_B(i, is_intermediate, k, T_list, A_0, A_1, m):
     p_ab = 0
-    for n in range(k):
-        p_abn = P_A_given_B_N(i, k, n, T_list, A_0, A_1)
-        p_ab += p_abn * comb(k - 1, n) * \
+    if is_intermediate:
+        n_range = k
+    else:
+        n_range = k + 1
+        
+    for n in range(n_range):
+        p_abn = P_A_given_B_N(i, is_intermediate, k, n, T_list, A_0, A_1)
+        p_ab += p_abn * comb(n_range - 1, n) * \
                 (m ** n) * \
-                ((1 - m) ** (k - 1 - n))
+                ((1 - m) ** (n_range - 1 - n))
     return p_ab
 
-def P_A(i, mean_degree, nodeN, T_list, m, A_0, A_1, k_max):    
+def P_A(i, is_intermediate, mean_degree, T_list, m, A, k_max):    
     pa_L = 0
     for k in range(1, k_max):
-        p_k = poisson.pmf(k, mean_degree)
-        p_b = k * p_k / mean_degree
-        p_ab = P_A_given_B(i, k, T_list, A_0, A_1, m)
-        pa_L += p_ab * p_b
+        prob_r = poisson.pmf(k, mean_degree)
+        if is_intermediate: # intermediate q using excess degree distribution
+            pb = prob_r * k * 1.0 / mean_degree 
+        else:
+            pb = prob_r
+#         p_b = k * p_k / mean_degree
+        p_ab = P_A_given_B(i, is_intermediate, k, T_list, A[0], A[1], m)
+        pa_L += p_ab * pb
     return pa_L
 
-def p_A_vec(mean_degree, nodeN, T_list, m, A_0, A_1, k_max):
-    A0 = P_A(0, mean_degree, nodeN, T_list, m, A_0, A_1, k_max)
-    A1 = P_A(1, mean_degree, nodeN, T_list, m, A_0, A_1, k_max)
+def pA_vec(mean_degree, is_intermediate, T_list, m, A, k_max):
+    A0 = P_A(0, is_intermediate, mean_degree, T_list, m, A, k_max)
+    A1 = P_A(1, is_intermediate, mean_degree, T_list, m, A, k_max)
     return [A0, A1]
 
-def func_fix(A, mean_degree, nodeN, T_list, m):
-    return np.array(p_A_vec(mean_degree, nodeN, T_list, m, A[0], A[1]))
 
-def func_root(A, mean_degree, nodeN, T_list, m, k_max):
-    return np.array(p_A_vec(mean_degree, nodeN, T_list, m, A[0], A[1], k_max)) - np.array(A)
+def func_root(A, mean_degree, T_list, m, k_max):
+    return np.array(pA_vec(mean_degree, True, T_list, m, A, k_max)) - np.array(A)
 
 def get_EpidemicSize(mean_degree, paras, infection_size0, infection_size1, infection_size):
     '''
@@ -82,60 +94,65 @@ def get_EpidemicSize(mean_degree, paras, infection_size0, infection_size1, infec
     init_A = (0.9, 0.9)
     m = paras.m
 
-    A_0_1_root = optimize.fsolve(func_root, init_A, args=(mean_degree, paras.n, T_list, m, k_max))
-    pa_L_0 = 0
-    pa_L_1 = 0
-    pa_L = 0
+    A_0_1_root = optimize.fsolve(func_root, init_A, args=(mean_degree, T_list, m, k_max))
+    A0, A1 = pA_vec(mean_degree, False,  T_list, paras.m, A_0_1_root, k_max)
+    A = A0 * paras.m + A1 * (1 - paras.m)
+    print(mean_degree, A, A0, A1)
+    infection_size0[mean_degree] = A0
+    infection_size1[mean_degree] = A1
+    infection_size[mean_degree]  = A
     
-    for k in range(0, k_max): # k now can start from 0, because for the last level the k could be 0 [0, kmax)
-        p_k = poisson.pmf(k, mean_degree) 
-        p_ab_0 = 0
-        p_ab_1 = 0
+    
+    
+#     for k in range(0, k_max): # k now can start from 0, because for the last level the k could be 0 [0, kmax)
+#         p_k = poisson.pmf(k, mean_degree) 
+#         p_ab_0 = 0
+#         p_ab_1 = 0
         
-        for n in range(k + 1): # n can be k
+#         for n in range(k + 1): # n can be k
        
-            p_abn_0 = 0
-            p_abn_1 = 0
+#             p_abn_0 = 0
+#             p_abn_1 = 0
             
-            for k0 in range(n + 1): # [0, n] 
-                for k1 in range(k - n + 1): # k1 can reach k - n
-                    p_a_given_r_0 = P_A_given_R(0, T_list, k0, k1)
-                    p_a_given_r_1 = P_A_given_R(1, T_list, k0, k1)
+#             for k0 in range(n + 1): # [0, n] 
+#                 for k1 in range(k - n + 1): # k1 can reach k - n
+#                     p_a_given_r_0 = P_A_given_R(0, T_list, k0, k1)
+#                     p_a_given_r_1 = P_A_given_R(1, T_list, k0, k1)
                     
-                    p_abn_0 += p_a_given_r_0 * \
-                             comb(n, k0) * \
-                             comb(k - n, k1) * \
-                             (A_0_1_root[0] ** k0) * \
-                             (A_0_1_root[1] ** k1) * \
-                             ((1 - A_0_1_root[0]) ** (n - k0)) * \
-                             ((1 - A_0_1_root[1]) ** (k - n - k1))
+#                     p_abn_0 += p_a_given_r_0 * \
+#                              comb(n, k0) * \
+#                              comb(k - n, k1) * \
+#                              (A_0_1_root[0] ** k0) * \
+#                              (A_0_1_root[1] ** k1) * \
+#                              ((1 - A_0_1_root[0]) ** (n - k0)) * \
+#                              ((1 - A_0_1_root[1]) ** (k - n - k1))
                     
-                    p_abn_1 += p_a_given_r_1 * \
-                             comb(n, k0) * \
-                             comb(k - n, k1) * \
-                             (A_0_1_root[0] ** k0) * \
-                             (A_0_1_root[1] ** k1) * \
-                             ((1 - A_0_1_root[0]) ** (n - k0)) * \
-                             ((1 - A_0_1_root[1]) ** (k - n - k1))
+#                     p_abn_1 += p_a_given_r_1 * \
+#                              comb(n, k0) * \
+#                              comb(k - n, k1) * \
+#                              (A_0_1_root[0] ** k0) * \
+#                              (A_0_1_root[1] ** k1) * \
+#                              ((1 - A_0_1_root[0]) ** (n - k0)) * \
+#                              ((1 - A_0_1_root[1]) ** (k - n - k1))
                     
-            p_ab_0 += p_abn_0 * comb(k, n) * \
-                    (m ** n) * \
-                    ((1 - m) ** (k - n))
-            p_ab_1 += p_abn_1 * comb(k, n) * \
-                    (m ** n) * \
-                    ((1 - m) ** (k - n))
+#             p_ab_0 += p_abn_0 * comb(k, n) * \
+#                     (m ** n) * \
+#                     ((1 - m) ** (k - n))
+#             p_ab_1 += p_abn_1 * comb(k, n) * \
+#                     (m ** n) * \
+#                     ((1 - m) ** (k - n))
             
             
-        pa_L_0 += p_k * p_ab_0
-        pa_L_1 += p_k * p_ab_1
-        pa_L += p_k * (m * p_ab_0 + (1 - m) * p_ab_1) 
+#         pa_L_0 += p_k * p_ab_0
+#         pa_L_1 += p_k * p_ab_1
+#         pa_L += p_k * (m * p_ab_0 + (1 - m) * p_ab_1) 
     
-    print(mean_degree, pa_L, pa_L_0, pa_L_1)
-    infection_size0[mean_degree] = pa_L_0
-    infection_size1[mean_degree] = pa_L_1
-    infection_size[mean_degree]  = pa_L
+#     print(mean_degree, pa_L, pa_L_0, pa_L_1)
+#     infection_size0[mean_degree] = pa_L_0
+#     infection_size1[mean_degree] = pa_L_1
+#     infection_size[mean_degree]  = pa_L
     
-    return pa_L_0, pa_L_1, pa_L, 1 - np.array(A_0_1_root)
+    return A0, A1, A
 
 ########### Mask Model PE Analysis -- Parellel ########### 
 def PE(i, is_intermediate, E0, E1, T_list, m, mean_degree, max_degree):
