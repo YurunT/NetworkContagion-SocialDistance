83c19443a56f9c8a993017d90760a3ad57130e7d
YurunT <944461466@qq.com>
2020-10-26 18:07:09 +0700

    mu pe es tnn done

--- analysis_scripts/change_m.sh
+++ analysis_scripts/change_m.sh
@@ -3,5 +3,5 @@ source activate ytian
 for p_mask in 0.45
 do
 	echo $p_mask
-    time python analysis.py -modelname mask -itemname pe -m $p_mask -T 0.6 -tm1 0.3 1 -tm2 0.7 1 -mind 1 -maxd 1 -ns 1 -nc 40 -change 0 -msg testtnn #-mdl  8 9 10
+    time python analysis.py -modelname mutation -itemname pe -m $p_mask -T 0.6 -tm1 0.3 1 -tm2 0.7 1 -mind 1 -maxd 1 -ns 1 -nc 40 -change 0 -msg testtnn #-mdl  8 9 10
 done
