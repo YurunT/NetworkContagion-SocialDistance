872959373c9e4616ab9f4dad3ed303cc420e4561
YurunT <944461466@qq.com>
2020-11-07 17:29:38 +0700

    varyingT figure

--- analysis_scripts/change_m.sh
+++ analysis_scripts/change_m.sh
@@ -5,8 +5,14 @@ source activate ytian
 # -itemname:  {'es', 'pe'}
 # -mdl: if need to specify a certian mean_degree_list, or please comment this option
 
-for p_mask in 0.45
+# for p_mask in 0.45
+# do
+# 	echo $p_mask
+#     time python analysis.py -modelname mask -itemname pe -m $p_mask -T 0.6 -tm1 0.3 -tm2 0.7 -mind 10 -maxd 10 -ns 1 -nc 40 -change 0 -msg changemdl_8to10 -mdl  8 9 10
+# done
+
+for T in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
 do
-	echo $p_mask
-    time python analysis.py -modelname mask -itemname pe -m $p_mask -T 0.6 -tm1 0.3 -tm2 0.7 -mind 10 -maxd 10 -ns 1 -nc 40 -change 0 -msg changemdl_8to10 -mdl  8 9 10
+	echo $T
+    time python analysis.py -modelname mask -itemname pe -m 0.45 -T $T -tm1 0.3 -tm2 0.7 -mind 5 -maxd 5 -ns 1 -nc 40 -change 1 -msg varyT # -mdl  8 9 10
 done
--- simulation_scripts/change_m_ray.sh
+++ simulation_scripts/change_m_ray.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 source activate ytian
-for p_mask in 0.45
+# for p_mask in 0.45
+# do
+# echo change m: $p_mask
+# time python sim-ray.py -modelname mask  -n 5000 -e 10000 -th 0.05 -m $p_mask -T 0.6 -tm1 0.3 -tm2 0.7 -mind 0 -maxd 11 -ns 50 -nc 40 -cp 1000 -change 0 -msg 0to11 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+# done
+
+for T in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
 do
-echo change m: $p_mask
-time python sim-ray.py -modelname mask  -n 5000 -e 10000 -th 0.05 -m $p_mask -T 0.6 -tm1 0.3 -tm2 0.7 -mind 0 -maxd 11 -ns 50 -nc 40 -cp 1000 -change 0 -msg 0to11 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+	echo $T
+    time python sim-ray.py  -modelname mask -n 5000 -e 10000 -m 0.45 -T $T -tm1 0.3 -tm2 0.7 -mind 5 -maxd 5 -ns 1 -nc 40 -change 1 -msg varyT # -mdl  8 9 10
 done
