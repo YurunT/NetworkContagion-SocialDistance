50e58e2d0b6978c13ca9b1df2f299ce18981e7be
YurunT <944461466@qq.com>
2020-11-23 22:40:09 +0700

    start 4 strategies

--- simulation_scripts/change_m_ray.sh
+++ simulation_scripts/change_m_ray.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 source activate ytian
-for p_mask in 0.45
-do
-echo change m: $p_mask
-time python sim-ray.py -modelname mask  -n 50000 -e 1000 -th 0.05 -m $p_mask 0.55 -T 0.6 -tm1 0.3 1 -tm2 0.7 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg pe_3tps_0to10 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
-done
+
+echo strategy0
+time python sim-ray.py -modelname mask  -n 500000 -e 1000 -th 0.05 -m 0 0 1 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg strategy0 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+
+echo strategy1
+time python sim-ray.py -modelname mask  -n 500000 -e 1000 -th 0.05 -m 1 0 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg strategy1 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+
+echo strategy2
+time python sim-ray.py -modelname mask  -n 500000 -e 1000 -th 0.05 -m 0 1 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg strategy2 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+
+echo strategy3
+time python sim-ray.py -modelname mask  -n 500000 -e 1000 -th 0.05 -m 0.5 0.5 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg strategy3 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
