918814aa2db1be984bdbb0f68a89cdd287218c88
YurunT <944461466@qq.com>
2020-11-24 13:57:27 +0700

    git rm -r pycache & add strategies to notebook and change_m.sh

--- analysis_scripts/change_m.sh
+++ analysis_scripts/change_m.sh
@@ -13,15 +13,9 @@ source activate ytian
 # done
 
 
-# for p_mask in 0.45
-# do
-# 	echo $p_mask
-#     time python analysis.py -modelname mask -itemname pe -m $p_mask  0.55 -T 0.6 -tm1  0.3 1 -tm2 0.7 1 -mind 0 -maxd 10 -ns 50 -nc 40 -change 0 -msg change_vec_pe #-mdl  8 9 10
-# done
 
-
-echo es_strategy0
-time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 0 0 1 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy0 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+# echo es_strategy0
+# time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 0 0 1 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy0 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
 
 echo es_strategy1
 time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 1 0 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy1 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
