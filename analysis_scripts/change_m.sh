665d671f0d278b0c1fcb930d4689f07e92244b74
YurunT <944461466@qq.com>
2020-11-29 19:47:27 +0700

    print es and pe theories for 3 strategies

--- analysis_scripts/change_m.sh
+++ analysis_scripts/change_m.sh
@@ -17,14 +17,14 @@ source activate ytian
 # echo es_strategy0
 # time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 0 0 1 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy0 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
 
-echo es_strategy1
-time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 1 0 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy1 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+# echo es_strategy1
+# time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 1 0 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy1 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
 
-echo es_strategy2
-time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 0 1 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy2 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+# echo es_strategy2
+# time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 0 1 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy2 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
 
-echo es_strategy3
-time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 0.5 0.5 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy3 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
+# echo es_strategy3
+# time python analysis.py -modelname mask -itemname es -n 500000 -e 1000 -th 0.05 -m 0.5 0.5 0 -T 0.6 -tm1 0.3 0.5 1 -tm2 0.2 0.5 1 -mind 0 -maxd 10 -ns 50 -nc 40 -cp 100 -change 0 -msg es_strategy3 #-mdl 2.6 2.64 2.68 2.72 2.76 2.8 2.84 2.88 2.92 2.96 3.0
 
 
 echo pe_strategy0
