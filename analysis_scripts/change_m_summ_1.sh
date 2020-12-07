6ae98e935706a7d86393a10b174fa289b9f1903f
YurunT <944461466@qq.com>
2020-12-07 17:39:54 +0700

    m0+m1=1

--- analysis_scripts/change_m_summ_1.sh
+++ analysis_scripts/change_m_summ_1.sh
@@ -7,9 +7,9 @@ m1=$(echo "1-$m0"|bc)
 echo m0: $m0
 echo m1: $m1
 
-time python analysis.py -modelname mask -itemname es -m $m0 $m1 -T 0.6 -tm1 0.3 0.5 -tm2 0.2 0.5 -mind 5 -maxd 5 -ns 1 -nc 40 -change 0 -msg es_check_m_impact_md5 #-mdl  8 9 10
+time python analysis.py -modelname mask -itemname es -m $m0 $m1 -T 0.6 -tm1 0.3 0.5 -tm2 0.2 0.5 -mind 8 -maxd 8 -ns 1 -nc 40 -change 0 -msg es_check_m_impact_md5_$m0 #-mdl  8 9 10
 
-time python analysis.py -modelname mask -itemname pe -m $m0 $m1 -T 0.6 -tm1 0.3 0.5 -tm2 0.2 0.5 -mind 5 -maxd 5 -ns 1 -nc 40 -change 0 -msg pe_check_m_impact_md5 #-mdl  8 9 10
+time python analysis.py -modelname mask -itemname pe -m $m0 $m1 -T 0.6 -tm1 0.3 0.5 -tm2 0.2 0.5 -mind 8 -maxd 8 -ns 1 -nc 40 -change 0 -msg pe_check_m_impact_md5_$m0 #-mdl  8 9 10
 
 done
 
