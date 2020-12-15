1b06d5a8e6022b9734cc69d1437d558cfae56cf4
YurunT <944461466@qq.com>
2020-12-15 14:27:03 +0700

    tm1=[0.6, 0.9] needs better match, tm1=[0.4, 0,6] and tm1=[0.2, 0.3] needs non zero sim res for PE

---
+++ analysis_scripts/change_m_summ_1-inout.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+source activate ytian
+
+
+for m0 in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+do
+
+m1=$(echo "1-$m0"|bc)
+
+echo ----------------EXP STARTS----------------
+echo m0: $m0
+echo m1: $m1
+
+
+# time python analysis.py -modelname mask -itemname es -m $m0 $m1 -T 0.6 -tm1 0.6 0.9 -tm2 0.9 0.6 -mind 10 -maxd 10 -ns 1 -nc 40 -change 0 -msg es_check_inout3_impact_md10_m2_0_$m0 #-mdl  8 9 10
+
+time python analysis.py -modelname mask -itemname pe -m $m0 $m1 -T 0.6 -tm1 0.3 0.4 -tm2 0.4 0.3 -mind 10 -maxd 10 -ns 1 -nc 40 -change 0 -msg pe_check_inout2_impact_md10_m2_0_$m0 #-mdl  8 9 10
+echo ----------------EXP ENDS----------------
+done
+
+
+
+# for m0 in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8
+# do
+# for m2 in 0.1
+# do
+# m1=$(echo "1-$m0-$m2"|bc)
+
+# echo ----------------EXP STARTS----------------
+# echo m0: $m0
+# echo m1: $m1
+# echo m2: $m2
+
+
+# time python analysis.py -modelname mask -itemname es -m $m0 $m1 $m2 -T 0.6 -tm1 0.2 0.3 1 -tm2 0.3 0.2 1 -mind 15 -maxd 15 -ns 1 -nc 40 -change 0 -msg es_check_inout_impact_md15_m2_0.1_$m0 #-mdl  8 9 10
+
+# time python analysis.py -modelname mask -itemname pe -m $m0 $m1 $m2 -T 0.6 -tm1 0.2 0.3 1 -tm2 0.3 0.2 1 -mind 15 -maxd 15 -ns 1 -nc 40 -change 0 -msg pe_check_inout_impact_md15_m2_0.1_$m0 #-mdl  8 9 10
+# echo ----------------EXP ENDS----------------
+# done
+# done
